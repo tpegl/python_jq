34d060bca022e3dd99b34be65b7633122812becd
vapour <thomas.pegler@proton.me>
2024-05-14 16:23:07 +0100

    fix: correct imports. Add .gitignore

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from path import path, Pathlike
+import os
+import sys
+
+from pathlib import Path
 
 from JSON import Parser
 
-def main(filepath: str | Pathlike):
+def main(filepath: str | os.PathLike):
     # Create an instance of the Parser class
     parser = Parser()    
     # Open the file using the path package
-    with open('./data/64KB.json') as data:
+    with open(filepath) as data:
         # Parse file into a useful format (i.e. dict for JSON/CSV)
-        parsed = Parser.parse(data)
+        parsed = parser.parse(data)
     pass
 
 if __name__ == '__main__':
-    main()
+    args = sys.argv
+    print(args)
+    main(args[1])
